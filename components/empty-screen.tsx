97cf4d45a58099c8058beeea7e920a6228e158ed
Shivam Singh <43898209+specialsyt@users.noreply.github.com>
2024-09-28 02:40:55 -0400

    Minor styling changes plus removing actions

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -8,11 +8,10 @@ export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-        <h1 className="text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-        </h1>
+        <h1 className="text-lg font-semibold">Welcome to ClarifyAI</h1>
         <p className="leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
+          This is a live survey tool leveragin the power of LLMs to maximize
+          user data extraction! Built with{' '}
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
           <ExternalLink href="https://sdk.vercel.ai">
             Vercel AI SDK
