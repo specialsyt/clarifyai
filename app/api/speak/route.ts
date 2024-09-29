ba67c0ca322d69a0b386a38740878faf3db84670
Shivam Singh <43898209+specialsyt@users.noreply.github.com>
2024-09-29 01:07:37 -0400

    adding option to use advanced openai tts models

--- app/api/speak/route.ts
+++ app/api/speak/route.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@deepgram/sdk'
 import { NextRequest, NextResponse } from 'next/server'
+import OpenAI from 'openai'
+import { SpeechCreateParams } from 'openai/resources/audio/speech'
 
 export const revalidate = 0
 
@@ -9,6 +11,10 @@ export const revalidate = 0
  * @returns {Promise<NextResponse>} A NextResponse with the streamable response
  */
 export async function POST(request: NextRequest) {
+  return deepgram_POST(request)
+}
+
+async function deepgram_POST(request: NextRequest) {
   // gotta use the request object to invalidate the cache every request :vomit:
   const url = request.url
   const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY ?? '')
@@ -31,4 +37,36 @@ export async function POST(request: NextRequest) {
   response.headers.set('Expires', '0')
 
   return response
+}
+
+async function openai_POST(request: NextRequest) {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+
+  const message = await request.json()
+  const voice: SpeechCreateParams['voice'] = 'nova'
+
+  const text = message.text
+
+  const response = await openai.audio.speech.create({
+    model: 'tts-1',
+    voice: voice,
+    input: message
+  })
+
+  const stream = await response.body
+  const headers = {
+    'Content-Type': 'audio/mpeg'
+  }
+
+  const nextResponse = new NextResponse(stream, { headers })
+  nextResponse.headers.set('Surrogate-Control', 'no-store')
+  nextResponse.headers.set(
+    'Cache-Control',
+    's-maxage=0, no-store, no-cache, must-revalidate, proxy-revalidate'
+  )
+  nextResponse.headers.set('Expires', '0')
+
+  return nextResponse
 }
