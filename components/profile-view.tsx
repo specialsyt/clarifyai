df318351ad14979fcfb28c764cfc53ed525f75c7
Roe, Elliot Benjamin <eroe7@gatech.edu>
2024-09-29 01:01:16 -0400

    feat: no dark mode

--- components/profile-view.tsx
+++ components/profile-view.tsx
@@ -5,6 +5,7 @@ import SurveyItem from './survey-item'
 import { Session, Survey, User } from '@/lib/types'
 import { useRouter } from 'next/navigation'
 import { getUser } from '@/app/login/actions'
+import { Button } from './ui/button'
 
 export default function ProfileView({
   user,
@@ -17,7 +18,7 @@ export default function ProfileView({
 
   return (
     <div className="lg:w-1/2 mx-auto flex justify-center items-center grow">
-      <div className="grow h-full items-center rounded-lg border bg-white px-8 py-8 my-4 shadow-md dark:bg-zinc-950 ">
+      <div className="grow h-full items-center rounded-lg border bg-white px-8 py-8 my-4 shadow-md">
         <div className="text-7xl py-4">Welcome, {user.firstName}!</div>
         <hr />
         {surveys.length == 0 ? <div>No Surveys Yet</div> : <></>}
@@ -26,15 +27,14 @@ export default function ProfileView({
             return <SurveyItem key={s.id} surveyInfo={s} />
           })}
           <div className="w-1/5 m-[20px] aspect-square">
-            <button
-              type="button"
-              className="w-full h-full flex flex-col items-center justify-center rounded-lg bg-zinc-950 hover:bg-zinc-600 shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 overflow-hidden"
+            <Button
+              className="w-full h-full"
               onClick={() => {
                 router.push('/new')
               }}
             >
-              <PlusIcon className="w-12 h-12  text-white" />
-            </button>
+              <PlusIcon className="w-12 h-12  text-primary-foreground" />
+            </Button>
           </div>
         </div>
       </div>
--- components/providers.tsx
+++ components/providers.tsx
@@ -1,17 +1,13 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { ThemeProviderProps } from 'next-themes/dist/types'
 import { SidebarProvider } from '@/lib/hooks/use-sidebar'
 import { TooltipProvider } from '@/components/ui/tooltip'
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider {...props}>
-      <SidebarProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </SidebarProvider>
-    </NextThemesProvider>
+    <SidebarProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+    </SidebarProvider>
   )
 }
