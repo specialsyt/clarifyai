1810efd05ad6d6f330bfbcae324efdf1392a7017
Roe, Elliot Benjamin <eroe7@gatech.edu>
2024-09-29 02:42:58 -0400

    feat: home page changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,24 @@ export default function Home() {
   return (
     <div className="max-w-6xl mx-auto px-4">
       <header className="text-center py-10">
-        <h1 className="text-4xl font-bold text-gray-800">Clarify AI</h1>
+        <div className="flex flex-row items-center justify-center space-x-4">
+          <img
+            src="/assets/clarifythink.png"
+            alt="thinking"
+            width={50}
+            height={50}
+            style={{ transform: 'scaleX(-1)' }}
+          />
+          <h1 className="text-4xl font-bold text-gray-800">
+            Clarify <span className="text-blue-500">AI</span>
+          </h1>
+          <img
+            src="/assets/clarifythumbsup.png"
+            alt="thumbs up"
+            width={50}
+            height={50}
+          />
+        </div>
       </header>
       <main className="flex flex-col gap-10">
         <section className="text-center py-16 bg-gray-100 rounded-lg">
@@ -13,9 +30,9 @@ export default function Home() {
             Discover a new way to conduct surveys and interviews
           </h2>
           <p className="text-xl text-gray-600 mb-6 max-w-2xl mx-auto">
-            Embrace the power of Clarify AI to autonomously conduct surveys that
-            extract the maximum amount of information from your users by asking
-            the right questions at the right time.
+            Embrace the power of Clarify to autonomously conduct semi-structured
+            interviews that extract the maximum amount of information from your
+            users by asking the right questions at the right time.
           </p>
           <Link href="/new">
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
